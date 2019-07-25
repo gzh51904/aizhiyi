6062c4e3ed96cc3e0b9567c0f32fdebdcdc21307
shijiu <604723258@qq.com>
2019-07-26 00:37:00 +0800

    第二天提交

---
+++ src/pages/Register/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import styles from '../../assets/scss/register.module.scss'
+import '../../assets/css/common/reset.css';
+import {api} from '../../utils/index.js';
+import ReactDOM from 'react-dom';
+import { Button } from 'element-react';
+import 'element-theme-default';
+class Register extends Component{
+    constructor(){
+        super();
+        this.state={
+
+        }
+    }
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+export default Register
