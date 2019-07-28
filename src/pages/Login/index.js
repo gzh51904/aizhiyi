5eb0b0f0426d0bbb606acbb30e317c1cb75f15b0
shijiu <604723258@qq.com>
2019-07-28 23:50:07 +0800

    7/28提交

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -8,10 +8,97 @@ class Login extends Component{
     constructor() {
         super();
         this.state = {
+            tel:'',
+            pass:'',
+            telSwitch:false,
+            passSwitch:false,
+        }
+        this.telChange=this.telChange.bind(this)
+        this.telOnblur=this.telOnblur.bind(this)
+    }
+    telChange(e){
+        this.setState({
+            tel:e.target.value
+        })
 
+    }
+    passChange(e){
+        this.setState({
+            pass:e.target.value
+        })
+    }
+    passOnblur(){
+        let pass = this.state.pass;
+        if(pass){
+            this.setState({
+                passSwitch:true
+            })
+            this.allOnBlur()
+        }else{
+            this.refs.dialogText.innerHTML='密码不能为空'
+            this.refs.wrapper.style.display='block'
+            this.setState({
+                passSwitch:false
+            })
+        }
+    }
+    telOnblur(){
+        let regExp4Phone = new RegExp("^[1][3-9]\\d{9}");
+        let tel = this.state.tel;
+        if(tel){
+            if(regExp4Phone.test(tel)){
+                this.setState({
+                    telSwitch:true
+                })
+                this.allOnBlur()
+            }else{
+                this.refs.dialogText.innerHTML='手机号格式错误'
+                this.refs.wrapper.style.display='block'
+                this.setState({
+                    telSwitch:false
+                })
+            }
+        }else{
+            this.refs.dialogText.innerHTML='手机号不能为空'
+            this.refs.wrapper.style.display='block'
         }
     }
 
+    allOnBlur(){
+        if(this.state.telSwitch
+            && this.state.passSwitch)
+            {
+                this.refs.massage.style.opacity='1'
+            }else{
+            this.refs.massage.style.opacity='0.4'
+            }
+    }
+
+    eyeOpen(){
+        this.refs.eyeOpen.style.display='none'
+        this.refs.eyeClose.style.display='block'
+        this.refs.password.type='text'
+    }
+    eyeClose(){
+        this.refs.eyeClose.style.display='none'
+        this.refs.eyeOpen.style.display='block'
+        this.refs.password.type='password'
+    }
+    removeMask(){
+        this.refs.wrapper.style.display='none';
+        this.refs.dialogText.innerHTML=''
+    }
+    async confirm(){
+        let username = this.state.tel;
+        let password = this.state.pass;
+        await api.get('',{
+            params:{
+            username,
+            password
+            }        
+         })
+
+    }
     render() {
         return (<div className={styles.Login}>
                     <div className={[`${styles.header}`,`clearfix`].join(' ')}>
@@ -34,12 +121,22 @@ class Login extends Component{
                             <ul className={styles.formBox}>
                                 <li className={styles.item}>
                                     <i className={styles.telIcon}></i>
-                                    <input type="text" placeholder='请输入手机号'/>
+                                    <input type="text" placeholder='请输入手机号'
+                                    value={this.state.tel}
+                                    onChange={this.telChange.bind(this)}
+                                    onBlur={this.telOnblur.bind(this)}
+                                    />
                                 </li>
                                 <li className={styles.item}>
                                     <i className={styles.passIcon}></i>
-                                    <input type="password" placeholder='请输入密码'/>
-                                    <i className={styles.eyeIcon}></i>
+                                    <input type="password" placeholder='请输入密码'
+                                    value={this.state.pass}
+                                    onChange={this.passChange.bind(this)}
+                                    onBlur={this.passOnblur.bind(this)}
+                                    ref='password'
+                                    />
+                                    <i className={styles.eyeOpen} ref='eyeOpen' onClick={this.eyeOpen.bind(this)}></i>
+                                    <i className={styles.eyeClose} ref='eyeClose' onClick={this.eyeClose.bind(this)}></i>
                                 </li>
                             </ul>
                             <div className={styles.forgetPass}>
@@ -51,7 +148,7 @@ class Login extends Component{
                                 </a>
                             </div>
                             <div className={styles.btn}>
-                                <a href="javascript:void(0)" className={styles.btn_1} ref="massage" >登录</a>
+                                <a href="javascript:void(0)" className={styles.btn_1} ref="massage" onClick={this.confirm.bind(this)}>登录</a>
                             </div>
                         </form>
                         <div className={styles.company}>
@@ -72,8 +169,7 @@ class Login extends Component{
                         </div>
                         <div className={styles.dialog}>
                             <span ref='dialogText'></span>
-                        {/* <a href="javascript:void(0)" onClick={this.removeMask.bind(this)}> */}
-                                <a href="javascript:void(0)">
+                                <a href="javascript:void(0)" onClick={this.removeMask.bind(this)}>
                                     <i>确定</i>
                                 </a>
                         </div>
